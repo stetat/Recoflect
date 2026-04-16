e2f386c7987635bce0bdf2fad8e1553e4c71a863
Yemelzhan <y_azimberdiyev@kbtu.kz>
2026-04-16 15:58:46 +0500

    changed the routes in the app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,27 @@ import {Home} from './components/home/home';
 import {AboutUs} from './components/about-us/about-us';
 import {Contacts} from './components/contacts/contacts';
 
+
+// pages (новые)
+import { Login } from './pages/login/login';
+import { Register} from './pages/register/register';
+import { Records } from './pages/records/records';
+import { Goals } from './pages/goals/goals';
+import { Profile } from './pages/profile/profile';
+
+
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+
   { path: 'home', component: Home},
-  { path: 'about', component: AboutUs},
-  { path: 'contacts', component: Contacts},
+  { path: 'login', component: Login},
+  { path: 'register', component: Register },
+
+  { path: 'records', component: Records},
+  { path: 'goals', component: Goals },
+  { path: 'profile', component: Profile},
+
+  { path: 'about', component: AboutUs },
+  { path: 'contacts', component: Contacts },
 ];
+
